32e6b3b013aea357ed033ec6150cd90b3688ac38
Şeyda Nur DEMİR <dmrsydnr@gmail.com>
2022-04-05 10:57:53 +0300

    Added authentication, spring security, jwt (in comment)

---
+++ services/user-service/src/main/java/com/youngadessi/demo/user/security/JwtTokenFilter.java
@@ -0,0 +1,24 @@
+package com.youngadessi.demo.user.security;
+/*
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+}
+*/
